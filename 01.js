92277f6eb5fef7c6037b08b1a660156cf5274b3e
jinya-aderureak <86933311+jinya-aderureak@users.noreply.github.com>
2021-07-26 00:28:31 +0900

    Update 01.js

--- 01.js
+++ 01.js
@@ -1,16 +1,19 @@
-function showPopup(event) {
-    target = document.getElementById("popup");
-
-    if (!event) var event = window.event;
-    if (!event.pageX) px = event.clientX + document.body.scrollLeft; else px = event.pageX;
-    if (!event.pageY) py = event.clientY + document.body.scrollTop; else py = event.pageY;
-
-      target.style.left = px+10 + "px";
-      target.style.top = py+5 + "px";
-      target.style.visibility = "visible";
+function popupImage() {
+    var popup = document.getElementById('js-popup');
+    if(!popup) return;
+  
+    var blackBg = document.getElementById('js-black-bg');
+    var closeBtn = document.getElementById('js-close-btn');
+    var showBtn = document.getElementById('js-show-popup');
+  
+    closePopUp(blackBg);
+    closePopUp(closeBtn);
+    closePopUp(showBtn);
+    function closePopUp(elem) {
+      if(!elem) return;
+      elem.addEventListener('click', function() {
+        popup.classList.toggle('is-show');
+      });
+    }
   }
-
-  function hidePopup() {
-    target = document.getElementById("popup");
-    target.style.visibility = "hidden";
-  }
+  popupImage();
